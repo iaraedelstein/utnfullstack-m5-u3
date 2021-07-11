f6aaf0c71e85918026028f30c2a6b0b2b37babec
iaraedelstein <edeliaru@gmail.com>
2021-07-11 17:13:12 -0300

    avs

--- src/components/Contador.js
+++ src/components/Contador.js
@@ -1,10 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './style.css';
 
 export default function Contador() {
   const contador = useSelector((state) => state.numero);
   const dispatch = useDispatch();
+
+  const [nombre, setNombre] = useState('Iara');
+  const handleChange = (e) => {
+    const newName = e.target.value;
+    setNombre(newName);
+  };
+
+  useEffect(() => {
+    console.log('Entró a useEffect');
+    setNombre(nombre.toUpperCase());
+  }, [nombre]);
+
   return (
     <div className="counter">
       <h1 className="counter__title">Contador</h1>
@@ -14,6 +26,9 @@ export default function Contador() {
         <button onClick={() => dispatch({ type: 'DECREMENTAR' })}>-</button>
         <button onClick={() => dispatch({ type: 'RESET' })}>Volver a 0</button>
       </div>
+
+      <label>Nombre</label>
+      <input onChange={(e) => handleChange(e)} value={nombre} />
     </div>
   );
 }
